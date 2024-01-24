c2de46cf5f13322e1ea7e9a647cae6404336b632
kashyapkurmilla <kashyapkurmilla@gmail.com>
2024-01-24 19:48:39 +0530

    mongo

---
+++ app6/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def generate_random_number():
+    # Generate a random number
+    random_number = random.randint(1, 100)
+
+    # Generate random background color in hex format
+    random_color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+    # Render the HTML template with the random number and background color
+    return render_template('index.html', number=random_number, color=random_color)
+
+if __name__ == '__main__':
+    # Run the Flask application on port 9090
+    app.run(host='0.0.0.0', port=9090)
